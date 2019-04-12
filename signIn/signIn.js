2003f671f654dda6508dd06e2a07c63088733461
Jennifer Harden <jkfout1@gmail.com>
2019-04-12 17:57:33 -0500

    fixed background image and cleaned up files

--- signIn/signIn.js
+++ signIn/signIn.js
@@ -6,14 +6,9 @@ $(document).ready (function() {
       projectId: "class-project-1-856d5",
       storageBucket: "class-project-1-856d5.appspot.com",
       messagingSenderId: "865560392389"
-      //clientId: "890867978712-umdoaue8kh6uirir05e4nc72u34om0l1.apps.googleusercontent.com"
+      
     };
-    /*scopes [
-      "profile",
-      "email",	
-      "openid"
-    ]
-    */
+   
     firebase.initializeApp(config);
     var database = firebase.database();
     var googleName = "";
@@ -27,7 +22,7 @@ $(document).ready (function() {
         var token = result.credential.accessToken;
         var user = result.user;
         if (result.user){
-          window.location.href = "http://www.w3schools.com";
+          window.location.href = "../index.html";
         }
         else{
           $("#passwordError").val("You couldn't be Google verified");
@@ -35,26 +30,7 @@ $(document).ready (function() {
         console.log(result);
         console.log(token);
       })
-    // firebase.auth().getRedirectResult().then(function(result) {
-    //   if (result.credential) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // ...
-    //   }
-    //   // The signed-in user info.
-    //   var user = result.user;
-    //   console.log(user);
-    // }).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   console.log(errorCode);
-    //   var errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   var email = error.email;
-    //   // The firebase.auth.AuthCredential type that was used.
-    //   var credential = error.credential;
-    //   // ...
-    // });
+    
   })
 
 
